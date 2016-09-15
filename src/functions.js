b0b01a1d831636f8596cb84dd773a2be0e07e5ac
awe <WiRai@users.noreply.github.com>
2016-09-15 22:55:27 +0200

    docstrings added + default STATIC_DIR

--- src/functions.js
+++ src/functions.js
@@ -1,10 +1,20 @@
 /* @flow */
+const path = require('path');
 const functions = {};
 
 module.exports = functions;
 
+/**
+ * @function refine_getContextTemplate
+ * @description Refines getContextTemplate to add a STATIC_DIR key.
+ * @returns {void} Void.
+ */
 functions.refine_getContextTemplate = (original: Function): Function => (): Object => {
+  if (!process.env.PRODUCT_DIR) {
+    throw new Error('PRODUCT_DIR not set.');
+  }
   const context = original();
-  context.STATIC_DIR = '';
+  // noFlow
+  context.STATIC_DIR = path.join(process.env.PRODUCT_DIR, 'DATA', 'statics');
   return context;
 };
